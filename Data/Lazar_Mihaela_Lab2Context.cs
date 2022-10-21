2e6a404e6fe35d6e468424c42ce418093afc1969
Miky Lazar <lazar_mihaela9@yahoo.com>
2022-10-21 15:05:00 +0300

    Add project files.

---
+++ Data/Lazar_Mihaela_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Lazar_Mihaela_Lab2.Models;
+
+namespace Lazar_Mihaela_Lab2.Data
+{
+    public class Lazar_Mihaela_Lab2Context : DbContext
+    {
+        public Lazar_Mihaela_Lab2Context (DbContextOptions<Lazar_Mihaela_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lazar_Mihaela_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
